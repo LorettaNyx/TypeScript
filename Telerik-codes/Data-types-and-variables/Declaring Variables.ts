de24f403a33bc043ebb6ac1ccdf198c0212a39f0
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-10-23 21:53:15 +0300

    Create Declaring Variables.ts

---
+++ Telerik-codes/Data-types-and-variables/Declaring Variables.ts
@@ -0,0 +1,16 @@
+//Declaring Variables
+
+  //  When declaring a variable we:
+    //    Specify its name (called identifier)
+      //  May give it an initial value
+    //The syntax is the following:
+
+//<var|let|const> <identifier> [:type] [= <initialization>];
+
+let emptyVariable : number;
+var height = 200;
+var classmate : string = "Pesho";
+var anotherEmptyVariable;
+let width : any = 300;
+const name = "Pesho";
+

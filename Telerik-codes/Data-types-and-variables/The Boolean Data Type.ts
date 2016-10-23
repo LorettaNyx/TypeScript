3aedf1028c2ebef69a143ba9cc55a97e8aa7f089
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-10-23 21:44:43 +0300

    Create The Boolean Data Type.ts

---
+++ Telerik-codes/Data-types-and-variables/The Boolean Data Type.ts
@@ -0,0 +1,10 @@
+//The Boolean Data Type
+let c = 1;
+let d = 2;
+let greaterAB = (c > d);
+console.log(greaterAB);  // false
+
+let equalA1 = (c === 1);
+console.log(equalA1);    // true
+
+console.log((c !== d) && (d > 0));

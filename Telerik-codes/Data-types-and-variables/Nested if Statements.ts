585e62f0040517ead90734146a975e4725c9ad57
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-10-23 22:03:18 +0300

    Create Nested if Statements.ts

---
+++ Telerik-codes/Data-types-and-variables/Nested if Statements.ts
@@ -0,0 +1,11 @@
+let first=5;
+let second=6;
+if (first === second) {
+    console.log("These two numbers are equal.");
+} else {
+    if (first > second) {
+        console.log("The first number is bigger.");
+    } else {
+        console.log("The second is bigger.");
+    }
+}

8c5a4f17f8602836de967334677c0c60deb90d0d
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-10-23 22:07:27 +0300

    Create Multiple Labels – Example.ts

---
+++ Telerik-codes/Data-types-and-variables/Multiple Labels – Example.ts
@@ -0,0 +1,18 @@
+let animal;
+switch (animal)
+{
+    case "dog" :
+        console.log("MAMMAL");
+        break;
+    case "crocodile" :
+    case "tortoise" :
+    case "snake" :
+        console.log("REPTILE");
+        break;
+    default :
+        console.log("There is no such animal!");
+        break;
+}
+
+//    You can use multiple labels to execute the same statement in more than one case
+

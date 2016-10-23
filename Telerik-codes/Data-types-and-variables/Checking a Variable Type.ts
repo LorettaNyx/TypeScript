0551d330efea314d236cec7aa96101e6f6f43840
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-10-23 21:51:20 +0300

    Create Checking a Variable Type.ts

---
+++ Telerik-codes/Data-types-and-variables/Checking a Variable Type.ts
@@ -0,0 +1,13 @@
+//Checking a Variable Type
+
+//    The variable type can be checked at runtime:
+
+let x = 5;
+console.log(typeof x); // number
+console.log(x); // 5
+
+x = null;
+console.log(typeof x); // object
+
+x = undefined;
+console.log(typeof x); // undefined

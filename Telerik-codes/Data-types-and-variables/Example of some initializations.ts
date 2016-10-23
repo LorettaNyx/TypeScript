34954e1f2c3187d45bc447a5e341e0ac36a692e3
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-10-23 21:57:03 +0300

    Create Example of some initializations.ts

---
+++ Telerik-codes/Data-types-and-variables/Example of some initializations.ts
@@ -0,0 +1,9 @@
+// This is how we use a literal expression:
+let heightInMeters = 1.74;
+
+// Here we use an already initialized variable:
+let greeting = 'Hello World!';
+let message = greeting;
+
+// Use a result from an expression
+const parsedNumber = parseInt('1239') + 1;

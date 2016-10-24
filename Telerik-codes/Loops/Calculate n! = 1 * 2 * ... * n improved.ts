419886517220336bce817752f3e8b702950589fd
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-10-24 10:43:13 +0300

    Create Calculate n! = 1 * 2 * ... * n improved.ts

---
+++ Telerik-codes/Loops/Calculate n! = 1 * 2 * ... * n improved.ts
@@ -0,0 +1,14 @@
+// Calculate n! = 1 * 2 * ... * n
+let n: number = 6;
+let result: number = 1;
+let logable = n;
+while (true) {
+    if (n === 1) {
+        break;
+    }
+
+    result *= n;
+    n--;
+}
+
+console.log(`${logable}! = ${result}`);

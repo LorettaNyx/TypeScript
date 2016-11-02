c0d868c8d9da4c71f81e580fba23783a7c0dae86
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-11-02 10:47:12 +0200

    Create Allocate array.js

---
+++ Homework-TS/Arrays/Allocate array.js
@@ -0,0 +1,13 @@
+function allocateArray(args) {
+	var i,
+		myArray = [],
+		result = '',
+		arraySize = +args[0];
+
+	for (i = 0; i < arraySize; i += 1) {
+		myArray[i] = i * 5;
+		result += myArray[i] + '\n';
+	}
+	
+	return result;
+}

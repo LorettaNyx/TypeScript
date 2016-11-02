c894c8ead805e286557559e2d70d31eeeaf9e9ee
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-11-02 10:42:48 +0200

    Create MMS.js

---
+++ Homework-TS/Loops/MMS.js
@@ -0,0 +1,23 @@
+function mmsa(args) {
+    var i,
+        average,
+        numbers = args.map(Number),
+        len = numbers.length,
+        min = numbers[0],
+        max = numbers[0],
+        sum = 0;
+
+    for (i = 0; i < len; i += 1) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+        if (numbers[i] < min) {
+            min = numbers[i];
+        }
+        sum += numbers[i];
+    }
+
+    average = sum / len;
+
+    return 'min=' + min.toFixed(2) + '\nmax=' + max.toFixed(2) + '\nsum=' + sum.toFixed(2) + '\navg=' + average.toFixed(2);
+}

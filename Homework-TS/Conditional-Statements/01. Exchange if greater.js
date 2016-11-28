e0584b104e4234c9893291d987bf08f87a1ff511
LorettaNyx <LorettaNyx@users.noreply.github.com>
2016-11-28 19:25:32 +0200

    Create 01. Exchange if greater.js

---
+++ Homework-TS/Conditional-Statements/01. Exchange if greater.js
@@ -0,0 +1,12 @@
+function solve(solve) {
+	var a = solve[0];
+  var b = solve[1];
+
+  if (a >= b) {
+      console.log(b + " " + a);
+  }
+  else {
+      console.log(a + " " + b);
+  }
+}
+solve(['3','2'])
